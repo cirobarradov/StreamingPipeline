28fea0665edcb6ae2241f87b115f0f6a5e985a8c
cirobarradov <rbravo79@gmail.com>
2018-06-12 20:07:08 +0200

    upload code

--- beam/src/main/java/com/ferrovial/digitalhub/twitter/Constants.java
+++ beam/src/main/java/com/ferrovial/digitalhub/twitter/Constants.java
@@ -2,5 +2,5 @@ package com.ferrovial.digitalhub.twitter;
 
 public class Constants {
     // Replace the accessKey string value with your valid access key.
-    public static String accessKey = "8459691d81fe429fbf1a8ed12ae0c646";
+    public static String accessKey = "PUT YOUR ACCESS KEY";
 }
