d46d2792ad6e075bed35105d02cfc8149fe98d4e
cirobarradov <rbravo79@gmail.com>
2018-06-24 12:50:23 +0200

    upload code

--- beam/src/main/java/com/ferrovial/digitalhub/twitter/TwitterUtils.java
+++ beam/src/main/java/com/ferrovial/digitalhub/twitter/TwitterUtils.java
@@ -79,6 +79,22 @@ public class TwitterUtils {
         return keys.trim();
     }
 
+    public static String getUserOrigin(String json)
+    {
+        String origin=null;
+        JsonNode tweet= parseJson(json);
+        JsonNode entities= (JsonNode) tweet.get("entities");
+        if (entities!=null)
+        {
+            ArrayNode mentions = (ArrayNode) entities.get("user_mentions");
+            if (mentions.size()>0)
+            {
+                origin = mentions.get(0).get("screen_name").asText();
+            }
+        }
+        return origin;
+    }
+
     /**
      * text -> (text, position, sentiment value, keywords)
      * @param json
@@ -158,6 +174,11 @@ public class TwitterUtils {
         res.put("keyPhrases", parseKeyPhrases(keyPhrasesJson));
         res.put("position", tweet.get("User").get("Location"));
         res.put("user", tweet.get("User").get("ScreenName"));
+        String origin=getUserOrigin(json);
+        if (origin!=null)
+        {
+            res.put("user-origin", origin);
+        }
         //res.put("json",json);
         /**
          * {"id":"1007950583351963650",
@@ -196,6 +217,152 @@ public class TwitterUtils {
         df.setTimeZone(tz);
         String nowAsISO = df.format(new Date());
         System.out.println(nowAsISO);
+        String json= "{\n" +
+                "      \"text\": \"RT @PostGradProblem: In preparation for the NFL lockout, I will be spending twice as much time analyzing my fantasy baseball team during ...\", \n" +
+                "      \"truncated\": true, \n" +
+                "      \"in_reply_to_user_id\": null, \n" +
+                "      \"in_reply_to_status_id\": null, \n" +
+                "      \"favorited\": false, \n" +
+                "      \"source\": \"<a href=\\\"http://twitter.com/\\\" rel=\\\"nofollow\\\">Twitter for iPhone</a>\", \n" +
+                "      \"in_reply_to_screen_name\": null, \n" +
+                "      \"in_reply_to_status_id_str\": null, \n" +
+                "      \"id_str\": \"54691802283900928\", \n" +
+                "      \"entities\": {\n" +
+                "            \"user_mentions\": [\n" +
+                "                  {\n" +
+                "                        \"indices\": [\n" +
+                "                              3, \n" +
+                "                              19\n" +
+                "                        ], \n" +
+                "                        \"screen_name\": \"PostGradProblem\", \n" +
+                "                        \"id_str\": \"271572434\", \n" +
+                "                        \"name\": \"PostGradProblems\", \n" +
+                "                        \"id\": 271572434\n" +
+                "                  }\n" +
+                "            ], \n" +
+                "            \"urls\": [ ], \n" +
+                "            \"hashtags\": [ ]\n" +
+                "      }, \n" +
+                "      \"contributors\": null, \n" +
+                "      \"retweeted\": false, \n" +
+                "      \"in_reply_to_user_id_str\": null, \n" +
+                "      \"place\": null, \n" +
+                "      \"retweet_count\": 4, \n" +
+                "      \"created_at\": \"Sun Apr 03 23:48:36 +0000 2011\", \n" +
+                "      \"retweeted_status\": {\n" +
+                "            \"text\": \"In preparation for the NFL lockout, I will be spending twice as much time analyzing my fantasy baseball team during company time. #PGP\", \n" +
+                "            \"truncated\": false, \n" +
+                "            \"in_reply_to_user_id\": null, \n" +
+                "            \"in_reply_to_status_id\": null, \n" +
+                "            \"favorited\": false, \n" +
+                "            \"source\": \"<a href=\\\"http://www.hootsuite.com\\\" rel=\\\"nofollow\\\">HootSuite</a>\", \n" +
+                "            \"in_reply_to_screen_name\": null, \n" +
+                "            \"in_reply_to_status_id_str\": null, \n" +
+                "            \"id_str\": \"54640519019642881\", \n" +
+                "            \"entities\": {\n" +
+                "                  \"user_mentions\": [ ], \n" +
+                "                  \"urls\": [ ], \n" +
+                "                  \"hashtags\": [\n" +
+                "                        {\n" +
+                "                              \"text\": \"PGP\", \n" +
+                "                              \"indices\": [\n" +
+                "                                    130, \n" +
+                "                                    134\n" +
+                "                              ]\n" +
+                "                        }\n" +
+                "                  ]\n" +
+                "            }, \n" +
+                "            \"contributors\": null, \n" +
+                "            \"retweeted\": false, \n" +
+                "            \"in_reply_to_user_id_str\": null, \n" +
+                "            \"place\": null, \n" +
+                "            \"retweet_count\": 4, \n" +
+                "            \"created_at\": \"Sun Apr 03 20:24:49 +0000 2011\", \n" +
+                "            \"user\": {\n" +
+                "                  \"notifications\": null, \n" +
+                "                  \"profile_use_background_image\": true, \n" +
+                "                  \"statuses_count\": 31, \n" +
+                "                  \"profile_background_color\": \"C0DEED\", \n" +
+                "                  \"followers_count\": 3066, \n" +
+                "                  \"profile_image_url\": \"http://a2.twimg.com/profile_images/1285770264/PGP_normal.jpg\", \n" +
+                "                  \"listed_count\": 6, \n" +
+                "                  \"profile_background_image_url\": \"http://a3.twimg.com/a/1301071706/images/themes/theme1/bg.png\", \n" +
+                "                  \"description\": \"\", \n" +
+                "                  \"screen_name\": \"PostGradProblem\", \n" +
+                "                  \"default_profile\": true, \n" +
+                "                  \"verified\": false, \n" +
+                "                  \"time_zone\": null, \n" +
+                "                  \"profile_text_color\": \"333333\", \n" +
+                "                  \"is_translator\": false, \n" +
+                "                  \"profile_sidebar_fill_color\": \"DDEEF6\", \n" +
+                "                  \"location\": \"\", \n" +
+                "                  \"id_str\": \"271572434\", \n" +
+                "                  \"default_profile_image\": false, \n" +
+                "                  \"profile_background_tile\": false, \n" +
+                "                  \"lang\": \"en\", \n" +
+                "                  \"friends_count\": 21, \n" +
+                "                  \"protected\": false, \n" +
+                "                  \"favourites_count\": 0, \n" +
+                "                  \"created_at\": \"Thu Mar 24 19:45:44 +0000 2011\", \n" +
+                "                  \"profile_link_color\": \"0084B4\", \n" +
+                "                  \"name\": \"PostGradProblems\", \n" +
+                "                  \"show_all_inline_media\": false, \n" +
+                "                  \"follow_request_sent\": null, \n" +
+                "                  \"geo_enabled\": false, \n" +
+                "                  \"profile_sidebar_border_color\": \"C0DEED\", \n" +
+                "                  \"url\": null, \n" +
+                "                  \"id\": 271572434, \n" +
+                "                  \"contributors_enabled\": false, \n" +
+                "                  \"following\": null, \n" +
+                "                  \"utc_offset\": null\n" +
+                "            }, \n" +
+                "            \"id\": 54640519019642880, \n" +
+                "            \"coordinates\": null, \n" +
+                "            \"geo\": null\n" +
+                "      }, \n" +
+                "      \"user\": {\n" +
+                "            \"notifications\": null, \n" +
+                "            \"profile_use_background_image\": true, \n" +
+                "            \"statuses_count\": 351, \n" +
+                "            \"profile_background_color\": \"C0DEED\", \n" +
+                "            \"followers_count\": 48, \n" +
+                "            \"profile_image_url\": \"http://a1.twimg.com/profile_images/455128973/gCsVUnofNqqyd6tdOGevROvko1_500_normal.jpg\", \n" +
+                "            \"listed_count\": 0, \n" +
+                "            \"profile_background_image_url\": \"http://a3.twimg.com/a/1300479984/images/themes/theme1/bg.png\", \n" +
+                "            \"description\": \"watcha doin in my waters?\", \n" +
+                "            \"screen_name\": \"OldGREG85\", \n" +
+                "            \"default_profile\": true, \n" +
+                "            \"verified\": false, \n" +
+                "            \"time_zone\": \"Hawaii\", \n" +
+                "            \"profile_text_color\": \"333333\", \n" +
+                "            \"is_translator\": false, \n" +
+                "            \"profile_sidebar_fill_color\": \"DDEEF6\", \n" +
+                "            \"location\": \"Texas\", \n" +
+                "            \"id_str\": \"80177619\", \n" +
+                "            \"default_profile_image\": false, \n" +
+                "            \"profile_background_tile\": false, \n" +
+                "            \"lang\": \"en\", \n" +
+                "            \"friends_count\": 81, \n" +
+                "            \"protected\": false, \n" +
+                "            \"favourites_count\": 0, \n" +
+                "            \"created_at\": \"Tue Oct 06 01:13:17 +0000 2009\", \n" +
+                "            \"profile_link_color\": \"0084B4\", \n" +
+                "            \"name\": \"GG\", \n" +
+                "            \"show_all_inline_media\": false, \n" +
+                "            \"follow_request_sent\": null, \n" +
+                "            \"geo_enabled\": false, \n" +
+                "            \"profile_sidebar_border_color\": \"C0DEED\", \n" +
+                "            \"url\": null, \n" +
+                "            \"id\": 80177619, \n" +
+                "            \"contributors_enabled\": false, \n" +
+                "            \"following\": null, \n" +
+                "            \"utc_offset\": -36000\n" +
+                "      }, \n" +
+                "      \"id\": 54691802283900930, \n" +
+                "      \"coordinates\": null, \n" +
+                "      \"geo\": null\n" +
+                "}";
+        getUserOrigin(json);
 
     }
 
