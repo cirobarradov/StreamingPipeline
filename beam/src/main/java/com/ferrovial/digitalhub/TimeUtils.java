7bbf28fe053a4f85e7e3546e5682d7c069aaae93
cirobarradov <rbravo79@gmail.com>
2018-07-02 18:15:38 +0200

    upload code

--- beam/src/main/java/com/ferrovial/digitalhub/TimeUtils.java
+++ beam/src/main/java/com/ferrovial/digitalhub/TimeUtils.java
@@ -4,6 +4,7 @@ import org.joda.time.Instant;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
 
@@ -14,6 +15,7 @@ public class TimeUtils {
         TimeZone tz = TimeZone.getTimeZone("Europe/Madrid");
         DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
         df.setTimeZone(tz);
+
         return df.format(instant.toDateTime().toDate());
     }
     public static String getTimestamp()
@@ -21,7 +23,10 @@ public class TimeUtils {
         TimeZone tz = TimeZone.getTimeZone("Europe/Madrid");
         DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
         df.setTimeZone(tz);
-
-        return df.format(new Date());
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.HOUR_OF_DAY, -2);
+        Date date = calendar.getTime();
+        return df.format(date);
     }
+
 }
